e6d379704d76ae78654c4a69efb4ee7b44384819
Talha A. B <talha.azim@gmail.com>
2021-08-25 20:26:13 +0000

    Memoized the next collatz function and added a main func

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
+class Memo(object):
+
+    cache_factory = dict
+
+    def __init__(self, fn):
+        self.fn = fn
+        self.cache = self.cache_factory()
+
+    def __call__(self, n):
+        return self.cache.setdefault(n, self.fn(n))
+
+    def __repr__(self):
+        return self.fn.__repr__()
+
+
+@Memo
 def collatzify(n):
     return 3 * n + 1 if n % 2 else n // 2
 
 
-def collatz(n, *, memo={2: 1}):
+def collatzseq(n):
     yield n
     while n != 1:
-        n = memo.setdefault(n, collatzify(n))
+        n = collatzify(n)
         yield n
 
 
-m = {i: collatzify(i) for i in range(1, 2022)}
+def main():
 
-a = [i for i in collatz(1987, memo=m)]
-b = [i for i in collatz(1993, memo=m)]
-c = [i for i in collatz(2016, memo=m)]
-d = [i for i in collatz(2021, memo=m)]
+    [collatzify(i) for i in range(2022)]
+    [fib(i) for i in range(10)]
 
-print(f'Calc saved {len(a)+ len(b) + len(c) + len(d) - len(m)}')
+    a = [i for i in collatzseq(1987)]
+    b = [i for i in collatzseq(1993)]
+    c = [i for i in collatzseq(2016)]
+    d = [i for i in collatzseq(2021)]
 
-print(len(a), len(b), len(c))
+    print(
+        f'{a[0]}: {len(a)}, {b[0]}: {len(b)}, {c[0]}: {len(c)}, {d[0]}: {len(d)}'
+    )
+
+
+if __name__ == "__main__":
+    main()
