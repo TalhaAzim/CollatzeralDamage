31400bb69633927a213dc66d0adf957df7f6c23b
talha <talha.azim@gmail.com>
2021-08-25 15:04:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+def collatzify(n):
+    return 3 * n + 1 if n % 2 else n // 2
+
+def collatz(n, *, memo={2: 1}):
+    yield n
+    while n != 1:
+        n = memo.setdefault(n, collatzify(n))
+        yield n
+
+
+m = {i: collatzify(i) for i in range(1, 2022)}
+
+a = [i for i in collatz(1987, memo=m)]
+b = [i for i in collatz(1993, memo=m)]
+c = [i for i in collatz(2016, memo=m)]
+d = [i for i in collatz(2021, memo=m)]
+
+print(f'Calc saved {len(a)+ len(b) + len(c) + len(d) - len(m)}')
+
+print(len(a), len(b), len(c))
